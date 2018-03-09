49984a72d0b933fd631dc2c934a8c4b1403beaa7
naga_pulivarthy <naga_pulivarthy@epam.com>
2018-03-09 16:56:49 +0530

    Power of Five Sample

---
+++ EngX0309/EngX0309/BinaryPowerofFiveCount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class BinaryPowerofFiveCount
+{
+    private static readonly string _pattern = @"101";
+    public static void Main()
+    {
+        Console.WriteLine("Please enter your input:");
+        var input = Console.ReadLine();
+
+        Console.Write("Matched Pattern Count : {0}", PowerofFiveCount(input));
+        Console.ReadKey();
+    }
+
+    public static int PowerofFiveCount(string input)
+    {
+        var regexp = new Regex(_pattern, RegexOptions.IgnoreCase);
+
+        var matchedCollection = regexp.Matches(input);
+        var matchCount = matchedCollection.Count;
+
+        return matchCount == 0 ? -1 : matchCount;
+    }
+}
---
+++ EngX0309/EngX0309Tests/BinaryPowerofFiveCountTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class BinaryPowerofFiveCountTests
+    {
+        private int expected;
+
+        private int actual;
+
+        private string input;
+
+        private PrivateType service = new PrivateType(typeof(BinaryPowerofFiveCount));
+
+        [TestMethod()]
+        public void PowerofFiveCount_returnsExpected_EmptyInput()
+        {
+            expected = -1;
+            input = "";
+
+            var service = new PrivateType(typeof(BinaryPowerofFiveCount));
+            actual = Convert.ToInt32(service.InvokeStatic("PowerofFiveCount", input));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PowerofFiveCount_returnsExpected_ValidInput()
+        {
+            expected = 3;
+            actual = 0;
+            input = "101101101";
+
+            actual = Convert.ToInt32(service.InvokeStatic("PowerofFiveCount", input));
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PowerofFiveCount_InvalidExpected_ShouldNotMatch()
+        {
+            expected = 3;
+            actual = 0;
+            input = "1111101";
+
+            actual = Convert.ToInt32(service.InvokeStatic("PowerofFiveCount", input));
+
+            Assert.AreNotEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PowerofFiveCount_returnsExpected_inValidInput()
+        {
+            expected = -1;
+            actual = 0;
+            input = "00000";
+
+            actual = Convert.ToInt32(service.InvokeStatic("PowerofFiveCount", input));
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
